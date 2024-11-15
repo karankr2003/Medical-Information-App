54dcfb27af88ac12cb37ef6f373716413e523c32
KARAN KUMAR <karankumar496kk@gmail.com>
2024-11-15 15:27:24 +0530

    first commit

---
+++ app/src/main/java/com/example/medic/dashboard.java
@@ -0,0 +1,27 @@
+package com.example.medic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class dashboard extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dashboard.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
